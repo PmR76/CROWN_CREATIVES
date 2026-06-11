b1049a1e9a6e65ca8c96a2f5cbc573f7594906ad
Pete <crowncreatives@outlook.com>
2026-06-11 21:29:14 +0100

    push - master

---
+++ assets/js/home.js
@@ -0,0 +1,7 @@
+// ============================================================
+// HOME PAGE ENGINE
+// ============================================================
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Home page engine loaded.");
+});
