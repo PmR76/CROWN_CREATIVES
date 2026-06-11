2f72418d4bfa57792194ee1744c8ae0d4c29b4c6
Pete <crowncreatives@outlook.com>
2026-06-11 10:54:07 +0100

    push

---
+++ master/start-master.cmd
@@ -0,0 +1,14 @@
+@echo off
+title Crown Creatives — Master Server
+
+echo Starting Crown Creatives Master System...
+echo -----------------------------------------
+echo.
+
+cd /d "%~dp0"
+
+start "" http://localhost:8080
+
+npx http-server . -p 8080
+
+pause
