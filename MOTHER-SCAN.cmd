f03aabf0ee323f5ba461e24870e16aedd9022333
Pete <crowncreatives@outlook.com>
2026-06-12 10:23:28 +0100

    push

--- MOTHER-SCAN.cmd
+++ MOTHER-SCAN.cmd
@@ -1,11 +1,8 @@
 @echo off
 title MOTHER v5 - Diagnostic Scan
 
-REM Move to the folder containing this CMD
+REM Move to the folder containing this CMD (CROWN_CREATIVES)
 cd /d "%~dp0"
-
-REM Move up TWO levels to reach CROWN_CREATIVES
-cd ../..
 
 REM Run the scanner from tools/mother
 node tools/mother/mother-v5.js
