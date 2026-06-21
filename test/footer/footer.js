9242d6d340869f0693d678fd2ae239c5a8a5f549
Pete <crowncreatives@outlook.com>
2026-06-21 16:51:46 +0100

    push

--- test/footer/footer.js
+++ test/footer/footer.js
@@ -33,15 +33,15 @@
       switch (id) {
 
         case "facebook":
-          window.open("https://facebook.com/crowncreatives", "_blank");
+          window.open("https://facebook.com/people/Crown-Creatives/61556386467490", "_blank");
           break;
 
         case "instagram":
-          window.open("https://instagram.com/crowncreatives", "_blank");
+          window.open("https://instagram.com/crown_creatives_uk", "_blank");
           break;
 
         case "email":
-          window.location.href = "mailto:crowncreatives@outlook.com";
+          window.location.href = "crowncreatives@outlook.com";
           break;
 
         case "copilot":
