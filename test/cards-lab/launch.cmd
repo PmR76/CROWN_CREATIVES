559587820649983bae888497ca0c132c34e633d4
Pete <crowncreatives@outlook.com>
2026-06-28 21:40:37 +0100

    push

--- test/cards-lab/launch.cmd
+++ test/cards-lab/launch.cmd
@@ -1,4 +1,8 @@
 @echo off
 cd /d "%~dp0"
-npx http-server -c-1 -p 5050 .
-start http://127.0.0.1:5050/<lab-crown-gallery>-lab.html
+
+REM Start local server inside THIS lab folder
+start "" npx http-server -c-1 -p 5050 .
+
+REM Launch Chrome directly to this lab
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" http://127.0.0.1:5050/cards-lab.html
