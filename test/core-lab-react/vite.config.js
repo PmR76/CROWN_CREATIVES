bbece8689c900c4ad8c35075e077d0545b573fc4
Pete <crowncreatives@outlook.com>
2026-07-01 23:15:28 +0100

    push

--- test/core-lab-react/vite.config.js
+++ test/core-lab-react/vite.config.js
@@ -4,9 +4,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
 server: {
-  host: "0.0.0.0",
-  port: 3000,
-  strictPort: true
+  port: 8080,
+  strictPort: true,
+  host: true
 }
 
 });
