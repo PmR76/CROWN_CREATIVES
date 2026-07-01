6f93f5cf2ea25b5c8445d21572829b52c965ca0c
Pete <crowncreatives@outlook.com>
2026-07-02 00:56:07 +0100

    push

--- test/core-lab-react/src/components/Ticker.jsx
+++ test/core-lab-react/src/components/Ticker.jsx
@@ -1,11 +1,11 @@
-import "./../styles/ticker.css";
+import "./ticker.css";
 
 export default function Ticker() {
   return (
-    <div id="cc-ticker" className="cc-ticker">
+    <div className="ticker">
       <div className="ticker-track">
-        <span>✦ CREATIVITY IS COURAGE ✦ IMAGINATION IS POWER ✦ ART IS FREEDOM ✦</span>
-        <span>✦ CREATIVITY IS COURAGE ✦ IMAGINATION IS POWER ✦ ART IS FREEDOM ✦</span>
+        <span>CREATIVITY IS COURAGE • IMAGINATION IS POWER • ART IS FREEDOM • </span>
+        <span>CREATIVITY IS COURAGE • IMAGINATION IS POWER • ART IS FREEDOM • </span>
       </div>
     </div>
   );
