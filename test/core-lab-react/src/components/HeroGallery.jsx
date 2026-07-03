fabb9394699bbff02c58abf6ce0a59bdf229d023
Pete <crowncreatives@outlook.com>
2026-07-03 22:25:36 +0100

    push

--- test/core-lab-react/src/components/HeroGallery.jsx
+++ test/core-lab-react/src/components/HeroGallery.jsx
@@ -9,13 +9,16 @@ export default function HeroGallery() {
 
   return (
     <>
-      <div className="hero-gallery-lane hero-gallery-left">
-        <img className="hero-gallery-img" src="" alt="" />
-      </div>
+    <div className="hero-gallery-container">
+  <div className="hero-gallery-left">
+    <img className="hero-gallery-img" />
+  </div>
 
-      <div className="hero-gallery-lane hero-gallery-right">
-        <img className="hero-gallery-img" src="" alt="" />
-      </div>
+  <div className="hero-gallery-right">
+    <img className="hero-gallery-img" />
+  </div>
+</div>
+
 
       <div className="hero-gallery-glow-overlay">
         <div className="hero-gallery-glow-left"></div>
