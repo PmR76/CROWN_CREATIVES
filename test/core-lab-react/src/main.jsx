938a3802024861875a781ae1c0a7ebbceb665b10
Pete <crowncreatives@outlook.com>
2026-07-06 02:47:34 +0100

    push

--- test/core-lab-react/src/main.jsx
+++ test/core-lab-react/src/main.jsx
@@ -6,6 +6,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+import "./styles/hero-crown.css";
+import "./styles/hero-gallery.css";
+import "./styles/footer.css";
+
+
 import "./styles/gradients.css";
 import "./shared/theme.css";
 
