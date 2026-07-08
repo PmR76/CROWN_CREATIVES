2a16dc7dddec3cd366a20928095593263d406d8f
Pete <crowncreatives@outlook.com>
2026-07-08 18:19:55 +0100

    push

--- sentinel/run-sentinel-manifest-scan.js
+++ sentinel/run-sentinel-manifest-scan.js
@@ -1,2 +1,2 @@
-import { runSentinelManifestScanner } from "./sentinel/SentinelManifestScanner.js";
+import { runSentinelManifestScanner } from "./SentinelManifestScanner.js";
 runSentinelManifestScanner();
