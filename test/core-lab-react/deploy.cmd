5aba7e5e985291286cde461edcb2c5bf1b5287c6
Pete <crowncreatives@outlook.com>
2026-07-09 23:06:08 +0100

    push

--- test/core-lab-react/deploy.cmd
+++ test/core-lab-react/deploy.cmd
@@ -13,6 +13,9 @@ echo  🔵 Launching Core Lab React (Vite Dev Server)
 echo  📁 Working Directory: %cd%
 echo.
 
+REM --- Auto-open browser (Chrome) ---
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" http://localhost:5173
+
 REM --- Start Vite dev server ---
 call npm run dev
 
