6e9aed27edaf37e6908f28a3094ec7a6e4f6adcd
Pete <crowncreatives@outlook.com>
2026-07-17 21:41:06 +0100

    push

--- test/core-lab-react/src/pages/Home.jsx
+++ test/core-lab-react/src/pages/Home.jsx
@@ -6,18 +6,24 @@
 import { useEffect, useState } from "react";
 
 import Background3D from "../components/background3D";
+import Header from "../components/Header";
+import HeroCrown from "../components/HeroCrown";
 import HeroGallery from "../components/HeroGallery";
 import FrostedCards from "../components/FrostedCards";
 import Ticker from "../components/Ticker";
+import Footer from "../components/Footer";
 
 // Sentinel v2.0
 import { runGallerySentinel } from "../sentinel/GallerySentinel";
 import { runMusicSentinel } from "../sentinel/MusicSentinel";
 
 import "../styles/background3d.css";
+import "../styles/header.css";
+import "../styles/hero-crown.css";
 import "../styles/hero-gallery.css";
 import "../styles/frosted-cards.css";
 import "../styles/ticker.css";
+import "../styles/footer.css";
 
 export default function Home() {
   const [sentinelStatus, setSentinelStatus] = useState("BOOTING");
