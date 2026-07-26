46f5d31fa573066cd5a028c73ff72ca7bd9ce1d9
Pete <crowncreatives@outlook.com>
2026-07-26 22:15:11 +0100

    mod registry

--- corelab/src/labs/moduleRegistry.js
+++ corelab/src/labs/moduleRegistry.js
@@ -1,9 +1,15 @@
+import HeaderLab from "./HeaderLab.jsx";
+import HeroCrownLab from "./HeroCrownLab.jsx";
+import GalleryLab from "./GalleryLab.jsx";
+import CardsLab from "./CardsLab.jsx";
+import TickerLab from "./TickerLab.jsx";
+import FooterLab from "./FooterLab.jsx";
+
 export const modules = {
-  header: () => import("./HeaderLab.jsx"),
-  crown: () => import("./HeroCrownLab.jsx"),
-  background: () => import("./BackgroundLab.jsx"),
-  gallery: () => import("./GalleryLab.jsx"),
-  cards: () => import("./CardsLab.jsx"),
-  ticker: () => import("./TickerLab.jsx"),
-  footer: () => import("./FooterLab.jsx")
+  header: HeaderLab,
+  crown: HeroCrownLab,
+  gallery: GalleryLab,
+  cards: CardsLab,
+  ticker: TickerLab,
+  footer: FooterLab,
 };
